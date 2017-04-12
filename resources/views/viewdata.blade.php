4fd1e1a9e87ee007569612039c4f759fc965f39c
marcoandrepedrosa <marco_pedrosa@hotmail.com>
2017-04-12 17:22:43 +0100

    Create view to show data on db

---
+++ resources/views/viewdata.blade.php
@@ -0,0 +1,30 @@
+<!-- app/views/viewdata.blade.php -->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Alert List</title>
+
+    <!-- CSS -->
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <style>
+        body { padding-top:50px; } /* add some padding to the top of our site */
+    </style>
+</head>
+<body class="container">
+<div class="col-sm-8 col-sm-offset-2">
+
+    <!-- ALERTS -->
+    <!-- loop over the alerts and show off some things -->
+    @foreach ($itsqd_mon_messages as $alert)
+
+        <!-- GET OUR BASIC ALERT INFORMATION -->
+        <h2>ID={{ $alert->message_id }} HOST={{ $alert->host }} SERVICE={{ $alert->service }} <small>OUTPUT={{ $alert->out_1 }}</small></h2>
+
+    @endforeach
+
+</div>
+</body>
+</html>
