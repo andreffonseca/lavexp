88e139ff77b28a99db0a75f12fc7c53dc1d6c097
marcoandrepedrosa <marco_pedrosa@hotmail.com>
2017-04-12 17:29:42 +0100

    Route to a new view

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// create our route, return a view file (app/views/viewdata.blade.php)
+// we will also send the records we want to the view
+Route::get('viewdata', function() {
+    return View::make('viewdata')
+        // all the bears (will also return the fish, trees, and picnics that belong to them)
+        ->with('itsqd_mon_messages', itsqd_mon_messages::all());//->with('trees', 'picnics'));
+});
+
 /* http://localhost/itsqd/v1/nmessage?id=1&plr=POLLER-PMS-MMK&t=Wed Apr 12 00:52:32 CEST 2017&tp=PROBLEM&s=WARNING&srv=MMK0060 - DB_Oracle_PSEM_AlertLog&h=RACPRD05&ip=RACPRD05.siege.red&g=0_DB_RAC_ORACLE_PROD&ot=WARNING: ALERTLOG. there are logs with errors. SBPSEM2_ORA-01555=( WARNING )&th=http%3A%2F%2F172.25.66.75%2Fthruk%2F%23cgi-bin%2Fextinfo.cgi%3Ftype%3D2%26host%3DRACPRD05%26service%3DDB_Oracle_PSEM_AlertLog&sc=http%3A%2F%2Fsitd_moss%2Fitsqd%2FWeb_part_page%2FHost_id.aspx%3Fhostname%3DRACPRD05%26alertid%3DMMK0060*/
 
 /*Route::get('itsqd/v1/nmessage','MonitorController@message');*/
