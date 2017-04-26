8876bb823b40de4ad355789441b5eb07812a5bf7
Marco Pedrosa <marcoandrepedrosa@github.com>
2017-04-26 04:18:54 +0100

    User Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,44 +8,16 @@
 <body>
 <div class="row">
         <div class="page-header">
-            <h2>{!! trans('site/user.login_to_account') !!}</h2>
+            <h2>Please login to your account</h2>
         </div>
     </div>
-
-    <div class="container-fluid">
-        <div class="row">
-            {!! Form::open(array('url' => url('auth/loginpage'), 'method' => 'post', 'files'=> true)) !!}
-            <div class="form-group  {{ $errors->has('user') ? 'has-error' : '' }}">
-                {!! Form::label('user', "Username", array('class' => 'control-label')) !!}
-                <div class="controls">
-                    {!! Form::text('user', null, array('class' => 'form-control')) !!}
-                    <span class="help-block">{{ $errors->first('user', ':message') }}</span>
-                </div>
-            </div>
-            <div class="form-group  {{ $errors->has('pass') ? 'has-error' : '' }}">
-                {!! Form::label('pass', "Password", array('class' => 'control-label')) !!}
-                <div class="controls">
-                    {!! Form::password('pass', array('class' => 'form-control')) !!}
-                    <span class="help-block">{{ $errors->first('pass', ':message') }}</span>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="remember"> Remember Me
-                        </label>
-                    </div>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    <button type="submit" class="btn btn-primary" style="margin-right: 15px;">
-                        Login
-                    </button>
-                </div>
-            </div>
-            {!! Form::close() !!}
-        </div>
+    <form action=url('loginpage') method="post">
+        <label for="username">Name:</label>
+        <input type="text" id="username" name="user"><br>
+        {{ $errors->has('user') ? 'has-error' : '' }}<br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="pass"><br>
+        {{ $errors->has('pass') ? 'has-error' : '' }}<br>
+        <input type="submit">
+    </form>
 </div>
