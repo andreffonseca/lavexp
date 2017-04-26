35f4cfac3d53532f83655da8bed1ae7c6f63ab8a
Marco Pedrosa <marcoandrepedrosa@github.com>
2017-04-26 04:05:19 +0100

    User Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,26 +5,47 @@
 <head>
 <title>Please Login</title>
 </head>
-<body><
+<body>
+<div class="row">
+        <div class="page-header">
+            <h2>{!! trans('site/user.login_to_account') !!}</h2>
+        </div>
+    </div>
 
-{{ Form::open(array('url' => 'loginpage')) }}
-<h1>Login</h1>
+    <div class="container-fluid">
+        <div class="row">
+            {!! Form::open(array('url' => url('auth/loginpage'), 'method' => 'post', 'files'=> true)) !!}
+            <div class="form-group  {{ $errors->has('user') ? 'has-error' : '' }}">
+                {!! Form::label('user', "Username", array('class' => 'control-label')) !!}
+                <div class="controls">
+                    {!! Form::text('user', null, array('class' => 'form-control')) !!}
+                    <span class="help-block">{{ $errors->first('user', ':message') }}</span>
+                </div>
+            </div>
+            <div class="form-group  {{ $errors->has('pass') ? 'has-error' : '' }}">
+                {!! Form::label('pass', "Password", array('class' => 'control-label')) !!}
+                <div class="controls">
+                    {!! Form::password('pass', array('class' => 'form-control')) !!}
+                    <span class="help-block">{{ $errors->first('pass', ':message') }}</span>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="remember"> Remember Me
+                        </label>
+                    </div>
+                </div>
+            </div>
 
-<!-- if there are login errors, show them here -->
-<p>
-    {{ $errors->first('user') }}
-    {{ $errors->first('pass') }}
-</p>
-
-<p>
-    {{ Form::label('user', 'Username') }}
-    {{ Form::text('user', Input::old('user'), array('placeholder' => 'awesome@awesome.com')) }}
-</p>
-
-<p>
-    {{ Form::label('pass', 'Password') }}
-    {{ Form::password('pass') }}
-</p>
-
-<p>{{ Form::submit('Submit!') }}</p>
-{{ Form::close() }}
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    <button type="submit" class="btn btn-primary" style="margin-right: 15px;">
+                        Login
+                    </button>
+                </div>
+            </div>
+            {!! Form::close() !!}
+        </div>
+</div>
