20285344e15e4b0c8c87b1183b32daffe3074f70
Marco Pedrosa <marcoandrepedrosa@github.com>
2017-04-26 04:32:20 +0100

    User Login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
 
 class UserController extends Controller {
     function __construct() {}
